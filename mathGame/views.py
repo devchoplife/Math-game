a96565e5257d054f97c27f5a0f9cb27ca7b6173f
devchoplife <dritedurojaiye@gmail.com>
2022-12-16 19:06:44 +0100

    Create views for all pages in the application

--- mathGame/views.py
+++ mathGame/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = "math-game.html"
+
+class mathGameView(TemplateView):
+    template_name = "math-game.html"
+
+class anagramGaneView(templateView):
+    template_name = "anagram-game.html"
